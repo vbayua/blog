c83403c5a16bd5a7d98bde7af7fbb59d004c2bef
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-09 14:30:04 +0700

    init 1

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $with = ['category', 'author'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // return view('posts');
+    return view('posts', [
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
+    ]);
+})->name('home');
+
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+    ]);
+})->name('category');
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts,
+        'categories' => Category::all()
+    ]);
+});
