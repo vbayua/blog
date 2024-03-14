c172eae3ae2284a0d8bd1222724545f6154e12af
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-14 10:19:25 +0700

    Publish pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,10 @@
 
         @if ($posts->count())
             <x-post-grid :posts="$posts" />
+
+            {{ $posts->links() }}
         @else
-                <p class="text-center">No posts yet. Please check back later.</p>
+            <p class="text-center">No posts yet. Please check back later.</p>
         @endif
     </main>
 </x-layout>
