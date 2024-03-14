0e384eb7819b11b21943a3ac23fd60cebc9a55a6
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-14 10:19:08 +0700

    Added pagination

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,20 @@ class DatabaseSeeder extends Seeder
             'name' => 'John Doe'
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(10)->create([
             'user_id' => $user->id
         ]);
 
         $user = User::factory()->create([
             'name' => 'Elizabeth'
+        ]);
+
+        Post::factory(10)->create([
+            'user_id' => $user->id
+        ]);
+        
+        $user = User::factory()->create([
+            'name' => 'Adam Levi'
         ]);
 
         Post::factory(5)->create([
