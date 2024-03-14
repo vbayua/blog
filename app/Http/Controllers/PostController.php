e3babe2f277717416f415d6e97ed13425217b5d6
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-14 10:42:04 +0700

    Paginate with query string

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,7 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(10),
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(10)->withQueryString(),
         ]);
     }
 
