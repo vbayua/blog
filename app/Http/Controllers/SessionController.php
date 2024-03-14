733b62bbb74e1b14a05bba50a9fdc4cd66426e5d
Viskal <viskal.bayu@gmail.com>
2024-03-14 23:03:35 +0700

    regenerate auth in login store

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -20,6 +20,7 @@ class SessionController extends Controller
         ]);
 
         if(auth()->attempt($attributes)) {
+            session()->regenerate();
             return redirect('/')->with('success','Welcome back!');
         }
 
