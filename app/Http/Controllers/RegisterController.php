b26f54b23b05c56b429442fbde9e40cde16b4662
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-14 11:38:21 +0700

    Store user function

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,13 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     //
-    function create()
+    public function create()
     {
         return view('register.create');
+    }
+
+    public function store()
+    {
+        // create user
+        // to debug: return request()->all()
+
+        $attributes = request()->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|max:100|min:3',
+            'email' => 'required|email|max:255',
+            'password' => 'required|max:255|min:7'
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
     }
 }
