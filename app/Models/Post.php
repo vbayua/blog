84399db3098b68e865435b0bb431e4da3ed09190
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-15 09:58:28 +0700

    Post Comment Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -16,19 +17,19 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        $query->when($filters['search'] ?? false, fn($query, $search) => 
-            $query->where(fn($query) => 
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->where(fn($query) =>
                 $query->where('title', 'like', '%' . $search . '%')
                     ->orWhere('body', 'like', '%' . $search . '%'))
         );
-        
-        $query->when($filters['category'] ?? false, fn($query, $category) => 
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
             $query->whereHas('category', function ($query) use ($category) {
                 $query->where('slug', $category);
             })
         );
 
-        $query->when($filters['author'] ?? false, fn($query, $author) => 
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
             $query->whereHas('author', function ($query) use ($author) {
                 $query->where('username', $author);
             })
@@ -43,5 +44,10 @@ class Post extends Model
     public function author(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
