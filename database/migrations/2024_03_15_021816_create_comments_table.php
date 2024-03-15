e72ed9d18b8c190f41ad40e3fd11a6c89a8f4578
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-15 09:32:17 +0700

    Create Constraints On Database

--- database/migrations/2024_03_15_021816_create_comments_table.php
+++ database/migrations/2024_03_15_021816_create_comments_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
+
+            // $table->foreign('post_id')->references('id')->on('posts')->cascadeOnDelete();
         });
     }
 
