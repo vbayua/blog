49fecc9b258a84b879b41522f88fb5327f290d0e
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-16 09:15:53 +0700

    Fix layout

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,23 @@
 @props(['comment'])
-<x-panel class="">
+<x-panel class="bg-gray-50">
     <article class="flex space-x-4">
         <div class="flex-shrink-0">
-            <img src="https://i.pravatar.cc/100?id={{ $comment->id }}" alt="" class="rounded-xl" width="45"
-                height="45">
+            <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60" class="rounded-xl">
         </div>
-        <div class="">
-            <header class="mb-4 space-y-2">
-                <div class="flex items-center justify-between">
-                    <div class="space-y-2">
-                        <h3 class="font-bold text-sm">{{ $comment->author->name }}</h3>
-                        <a href="#" class="text-xs">{{ $comment->author->username }}</a>
-                    </div>
-                    <p class="text-xs text-gray-500"><time> {{ $comment->created_at->diffForHumans() }} </time></p>
-                </div>
-                <p class="text-sm">
-                    {{ $comment->body }}
+
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
                 </p>
             </header>
+
+            <p>
+                {{ $comment->body }}
+            </p>
         </div>
     </article>
 </x-panel>
