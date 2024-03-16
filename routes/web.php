ea2c0ada86bca5f08101081e6fe7cc06960fc884
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-16 09:01:00 +0700

    Add Post Comments Resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
@@ -18,6 +19,9 @@ use App\Http\Controllers\SessionController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class,'show'])->name('post');
+
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class,'store']);
+
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
