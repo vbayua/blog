0cc66840be0ec0eca53c469e5ca0724f0860fc74
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-17 12:01:52 +0700

    Single invoke newsletter controller & basic formatting

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Newsletter;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class NewsletterController extends Controller
+{
+    public function __invoke(Request $request, Newsletter $newsletter)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        // dd($request->input('email'));
+        try {
+            $newsletter->subscribe((string) $request->email);
+        } catch (\Exception $e) {
+            throw ValidationException::withMessages(['email' => 'Email could not be added to our newsletter list']);
+        }
+
+        return redirect('/')->with('success', 'You are now signed up for our newsletter');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
-use App\Services\Newsletter;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\NewsletterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,24 +17,13 @@ use Illuminate\Validation\ValidationException;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::post('newsletter', function (Request $request, Newsletter $newsletter) {
-    $request->validate([
-        'email' => 'required|email',
-    ]);
 
-    // dd($request->input('email'));
-    try {
-        $newsletter->subscribe((string) $request->email);
-    } catch (\Exception $e) {
-        throw ValidationException::withMessages(['email' => 'Email could not be added to our newsletter list']);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter');
-});
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('post');
 
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
@@ -46,17 +33,3 @@ Route::get('login', [SessionController::class, 'create'])
     ->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
-
-// Route::get('categories/{category:slug}', function (Category $category) {
-//     return view('posts', [
-//         'posts' => $category->posts,
-//         'currentCategory' => $category,
-//         'categories' => Category::all(),
-//     ]);
-// })->name('category');
-
-// Route::get('authors/{author:username}', function (User $author) {
-//     return view('posts.index', [
-//         'posts' => $author->posts,
-//     ]);
-// });
