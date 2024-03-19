20aac2d42d42aebe80057c7f82c80076889da38e
Viskal Bayu <viskal.bayu@gmail.com>
2024-03-19 11:48:21 +0700

    Tweak style

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,7 +2,7 @@
 
 <x-form.label name="{{ $name }}" />
 
-<input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" value="{{ old($name) }}"
-    class="border border-gray-200 rounded p-2 w-full" {{$attributes}}>
+<input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}"
+    class="border border-gray-200 rounded p-2 w-full" {{ $attributes(['value' => old($name)]) }}>
 
-<x-form.error name={{ $name }} />
+<x-form.error name="{{ $name }}" />
--- resources/views/components/settings.blade.php
+++ resources/views/components/settings.blade.php
@@ -1,7 +1,8 @@
 @props(['heading'])
-<section class="max-w-4xl mx-auto py-6">
+<section class="flex flex-col mx-auto py-6">
+
     <h1 class="text-lg mb-4 font-bold">
-        {{$heading}}
+        {{ $heading }}
     </h1>
 
     <div class="flex">
@@ -9,16 +10,18 @@
             <h4 class="font-semibold mb-4">Links</h4>
             <ul>
                 <li>
-                    <a href="/admin/dashboard" class="{{request()->is('admin/dashboard') ? 'text-blue-500' : ''}}">Dashboard</a>
+                    <a href="/admin/posts"
+                        class="{{ request()->is('admin/posts') ? 'text-blue-500' : '' }}">All Posts</a>
                 </li>
                 <li>
-                    <a href="/admin/posts/create" class="{{request()->is('admin/posts/create') ? 'text-blue-500' : ''}}">New Post</a>
+                    <a href="/admin/posts/create"
+                        class="{{ request()->is('admin/posts/create') ? 'text-blue-500' : '' }}">New Post</a>
                 </li>
             </ul>
         </aside>
         <main class="flex-1">
             <x-panel class="">
-                {{$slot}}
+                {{ $slot }}
             </x-panel>
         </main>
     </div>
